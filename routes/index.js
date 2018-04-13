f208e0ab20497d9c8f1b12b6122e44a4e71375a5
Wei Yuan <475214939@qq.com>
2018-04-13 16:31:42 +0800

    '实现留言功能'

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var crypto = require('crypto'), // crypto 是 Node.js 的一个核心模块，我们用它生成散列值来加密密码
     User = require('../models/user.js'),
-    Post = require('../models/post.js');;
+    Post = require('../models/post.js'),
+    Comment = require('../models/comment.js');;
 
 var router = express.Router();
 
@@ -248,6 +249,28 @@ router.get('/remove/:name/:day/:title', function (req, res) {
         }
         req.flash('success', '删除成功!');
         res.redirect('/');
+    });
+});
+
+router.post('/u/:name/:day/:title', function (req, res) {
+    var date = new Date(),
+        time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+    var comment = {
+        name: req.body.name,
+        email: req.body.email,
+        website: req.body.website,
+        time: time,
+        content: req.body.content
+    };
+    var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newComment.save(function (err) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('back');
+        }
+        req.flash('success', '留言成功!');
+        res.redirect('back');
     });
 });
 
